8b2e05fd11b2bb802faaa2e1668dc5ecdaaba489
Will <william.y.kuang@gmail.com>
2018-03-04 12:08:33 +1100

    refactor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import "../StarRating/StarRating";
 import StarRating from "../StarRating/StarRating";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: []
+    };
+  }
+
   render() {
+    const { colors } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <StarRating starsSelected="2" />
+        <AddColorForm />
+        <ColorList colors={colors} />
       </div>
     );
   }
--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -1,39 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import Star from "../Star/Star";
 import PropTypes from "prop-types";
 
-class StarRating extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      starsSelected: props.starsSelected || 0
-    };
-    this.change = this.change.bind(this);
-  }
-
-  change(starsSelected) {
-    this.setState({ starsSelected });
-  }
-
-  render() {
-    const { totalStars } = this.props;
-    const { starsSelected } = this.state;
-    return (
-      <div className="star-rating">
-        {[...Array(totalStars)].map((n, i) => (
-          <Star
-            key={i}
-            selected={i < starsSelected}
-            onClick={() => this.change(i + 1)}
-          />
-        ))}
-        <p>
-          {starsSelected} of {totalStars} stars
-        </p>
-      </div>
-    );
-  }
-}
+const StarRating = ({ starsSelected = 0, totalStars = 5, onRate = f => f }) => (
+  <div className="star-rating">
+    {[...Array(totalStars)].map((n, i) => (
+      <Star
+        key={i}
+        selected={i < starsSelected}
+        onClick={() => onRate(i + 1)}
+      />
+    ))}
+    <p>
+      {starsSelected} of {totalStars} stars
+    </p>
+  </div>
+);
 
 StarRating.propTypes = {
   totalStars: PropTypes.number
